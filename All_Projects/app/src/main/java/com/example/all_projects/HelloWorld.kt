9a5b740bccff195161b1ce57863422c9fd20da2b
Garvitjoshi1 <garvitjoshi46@gmail.com>
2024-02-17 19:35:17 +0530

    Android-Development-Learning

--- All_Projects/app/src/main/java/com/example/all_projects/HelloWorld.kt
+++ All_Projects/app/src/main/java/com/example/all_projects/HelloWorld.kt
@@ -3,11 +3,10 @@ package com.example.all_projects
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
+class HelloWorld : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_hello_world)
     }
 
     /*
--- All_Projects/app/src/main/java/com/example/all_projects/SolvingErrors.kt
+++ All_Projects/app/src/main/java/com/example/all_projects/SolvingErrors.kt
@@ -1,12 +1,30 @@
-package com.example.helloworld
+package com.example.all_projects
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.all_projects.R
+import java.util.Collections
 
 class SolvingErrors : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_solving_errors)
+
+        val list = listOf(5, 3, 6, 7, 9, 1)
+        sortList(list)
+        print(list)
+
+    }
+
+
+    private fun sortList(list: List<Int>){
+        for(i in 0..list.size - 1){
+            for(j in 0..list.size - 1){
+                if(list[j] > list[j+1]){
+                    Collections.swap(list, j, j+1)
+                }
+            }
+        }
     }
 }
